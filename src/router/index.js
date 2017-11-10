bf3e89e5510926551b2d082b84ae4832df24f0df
linjituan <linjituan@foresee.com.cn>
2017-11-10 15:58:31 +0800

    lizi

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import content from '@/page/content'
 import index from '@/page/index'
+import badge from '@/page/badge'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: index
-    },
-    {
-      path: '/content/:id',
-      name: 'content',
-      component: content
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'index',
+            component: index
+        },
+        {
+            path: '/content/:id',
+            name: 'content',
+            component: content
+        },
+        {
+            path: '/badge',
+            name: 'content',
+            component: badge
+        }
+    ]
 })
